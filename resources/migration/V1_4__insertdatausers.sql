97a53959284c770bde104bab9ddfa2bf754362d4
Payal Bansal <jahanvi.bansal@gmail.com>
2018-10-18 18:06:47 +0530

    added encoded value

--- resources/migration/V1_4__insertdatausers.sql
+++ resources/migration/V1_4__insertdatausers.sql
@@ -1,12 +1,12 @@
 insert into users ( username, password)
-values ( 'payal', 'payal123');
+values ( 'payal', '$2a$12$YcoYj8Si2mbx.gYTLWwPeu51cfI2bTJlWBnnpaI2uYitfQtKzjPxm');
 
 insert into users ( username, password)
-values ( 'admin', 'admin');
+values ( 'admin', '$2a$12$vhk1ELFdkwuvtAb8HrnUzOHEGJsnqX5ZX.C3TV3Q4Vuu9dsDcRH8e');
 
 insert into roles ( username, authority)
-values (  'payal', 'user');
+values (  'payal', 'ROLE_USER');
 
 insert into roles ( username, authority)
-values ( 'admin', 'admin');
+values ( 'admin', 'ROLE_ADMIN');
 
