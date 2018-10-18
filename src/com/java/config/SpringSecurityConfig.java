a1d93c19d6c2f5c2d3b58405ca31f8ace2f5e60c
Payal Bansal <jahanvi.bansal@gmail.com>
2018-10-18 18:05:57 +0530

    added encoder

--- src/com/java/config/SpringSecurityConfig.java
+++ src/com/java/config/SpringSecurityConfig.java
@@ -3,11 +3,13 @@ package com.java.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,22 +18,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource ds;
 
+	@Bean
+	public BCryptPasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder(12);
+	}
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(ds)
-				.usersByUsernameQuery("select username, password from users where username = ?")
-				.authoritiesByUsernameQuery("select  username, authority from roles where username = ?");
-		System.out.println("fired!" + ds);
-
+		auth.jdbcAuthentication().dataSource(ds).usersByUsernameQuery("select * from users where username = ?")
+				.authoritiesByUsernameQuery("select username, authority from roles where username = ?")
+				.passwordEncoder(getEncoder());
+		;
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// Spring Security 4 automatically prefixes any role with ROLE_.
-		http.authorizeRequests().
-		antMatchers("/").permitAll().
-		anyRequest().hasAnyAuthority("ROLE_ADMIN","ROLE_USER").anyRequest().authenticated()
-			.and().httpBasic();
+		http.authorizeRequests().antMatchers("/").permitAll().anyRequest()
+				.hasAnyRole("ADMIN","USER").anyRequest().authenticated().and().httpBasic();
 
 	}
 
