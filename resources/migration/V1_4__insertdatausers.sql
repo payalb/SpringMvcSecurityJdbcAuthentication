d26798772afa28b18fbbfbde624f635598b8e071
Payal Bansal <jahanvi.bansal@gmail.com>
2018-10-19 09:25:36 +0530

    Update V1_4__insertdatausers.sql

--- resources/migration/V1_4__insertdatausers.sql
+++ resources/migration/V1_4__insertdatausers.sql
@@ -1,12 +1,12 @@
 insert into users ( username, password)
-values ( 'payal', '$2a$12$YcoYj8Si2mbx.gYTLWwPeu51cfI2bTJlWBnnpaI2uYitfQtKzjPxm');
+values ( 'payal', '{bcrypt}$2a$12$dlm6tmvHvP1ObrL02vE3A.9qB8Uq.Fm/TIQrs6Vt/277YQNES4/UG');
 
 insert into users ( username, password)
-values ( 'admin', '$2a$12$vhk1ELFdkwuvtAb8HrnUzOHEGJsnqX5ZX.C3TV3Q4Vuu9dsDcRH8e');
+values ( 'admin', '{bcrypt}$2a$12$Vz1z1Zv9vOvsG43k1/3touiUf0HZaIYxJaP3luFPgxz/H4B1SCogy');
 
-insert into roles ( username, authority)
+insert into authorities ( username, authority)
 values (  'payal', 'ROLE_USER');
 
-insert into roles ( username, authority)
+insert into authorities ( username, authority)
 values ( 'admin', 'ROLE_ADMIN');
 
